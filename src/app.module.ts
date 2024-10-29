bb6635df34b7c9dd364fc4629c14144f8931c41d
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2024-10-29 06:46:13 -0500

    Add path mongoose connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { ArchivosModule } from './archivos/archivos.module';
 import { AudiosModule } from './audios/audios.module';
 import { PantallasModule } from './pantallas/pantallas.module';
 import { GrabacionesModule } from './grabaciones/grabaciones.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ArchivosModule, AudiosModule, PantallasModule, GrabacionesModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://cbricenoojeda42:GMAPA0ZTviZLWqyg@cluster0.avxwc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+    ArchivosModule,
+    AudiosModule,
+    PantallasModule,
+    GrabacionesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
