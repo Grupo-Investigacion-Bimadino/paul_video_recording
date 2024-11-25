d1d7250939af6e88fbfd99a6dada96d35bd75ea4
Yerson David Moreno Zapata <134532548+ElReyDavid2023@users.noreply.github.com>
2024-11-25 03:37:13 +0000

    maints

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {SwaggerTheme, SwaggerThemeNameEnum} from 'swagger-themes'
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle("API Documentation Video Recording")
+  .setDescription("Api documentation")
+  .setVersion("1.0")
+  .build()
+
+  const documentFactory = ()=> SwaggerModule.createDocument(app, config);
+
+  const theme = new SwaggerTheme();
+
+  const options = {
+    explorer:true,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.MONOKAI)  
+  }
+
+  SwaggerModule.setup('api', app, documentFactory, options)
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap();;
