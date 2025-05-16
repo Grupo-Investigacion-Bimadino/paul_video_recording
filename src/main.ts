0d2c51debab12fe7db8123bb12006a029ebcfb86
Cesar Briceño <134397257+cesarbriceno@users.noreply.github.com>
2025-05-16 23:40:53 +0000

    se creo la conexion on el fronted

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {SwaggerTheme, SwaggerThemeNameEnum} from 'swagger-themes'
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  const config = new DocumentBuilder()
-  .setTitle("API Documentation Video Recording")
-  .setDescription("Api documentation")
-  .setVersion("1.0")
-  .build()
 
-  const documentFactory = ()=> SwaggerModule.createDocument(app, config);
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*', // Permite cualquier origen en desarrollo. En producción, usa la URL del frontend (ej. 'http://localhost:4000')
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation Video Recording')
+    .setDescription('Api documentation')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
 
   const theme = new SwaggerTheme();
 
   const options = {
-    explorer:true,
-    customCss: theme.getBuffer(SwaggerThemeNameEnum.MONOKAI)  
-  }
+    explorer: true,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.MONOKAI),
+  };
 
-  SwaggerModule.setup('api', app, documentFactory, options)
+  SwaggerModule.setup('api', app, documentFactory, options);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();;
+bootstrap();
